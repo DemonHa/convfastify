3f47ac4154e465641a998c97f9e6c358c43ec226
DemonHa <hh360284@gmail.com>
2023-10-16 21:09:44 +0200

    feat: api documentation

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,64 @@
 import fp from "fastify-plugin";
 import { glob } from "glob";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastifySwagger from "@fastify/swagger";
 
-const convfastify = fp(
+const convfastify = fp<{ pattern: string }>(
   async (fastify, opt) => {
-    const options = opt as Partial<{ pattern: string }>;
-    if (!options.pattern) throw "Please specify a pattern to load files";
-    const routePaths = glob.sync(options.pattern, { absolute: true });
+    if (!opt.pattern) throw "Please specify a pattern to load files";
+    const routePaths = glob.sync(opt.pattern, { absolute: true });
 
-    for (const routePath of routePaths) {
-      const route = require(routePath);
-      fastify.route(route.default);
-    }
+    fastify.register(fastifySwagger, {
+      openapi: {
+        info: {
+          title: "Open API",
+          description: "Open API Documentation",
+          version: "1.0.0",
+        },
+        servers: [
+          {
+            url: "http://localhost",
+          },
+        ],
+        components: {
+          securitySchemes: {
+            bearerAuth: {
+              type: "http",
+              scheme: "bearer",
+            },
+          },
+        },
+      },
+    });
+
+    fastify.register(fastifySwaggerUi, {
+      routePrefix: "/docs",
+      uiConfig: {
+        docExpansion: "full",
+        deepLinking: false,
+      },
+      uiHooks: {
+        onRequest: function (_request, _reply, next) {
+          next();
+        },
+        preHandler: function (_request, _reply, next) {
+          next();
+        },
+      },
+      staticCSP: true,
+      transformStaticCSP: (header) => header,
+      transformSpecification: (swaggerObject) => {
+        return swaggerObject;
+      },
+      transformSpecificationClone: true,
+    });
+
+    fastify.register(async (fastify) => {
+      for (const routePath of routePaths) {
+        const route = require(routePath);
+        fastify.route(route.default);
+      }
+    });
   },
   {
     fastify: "4.x",
